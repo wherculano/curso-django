6fd5b85ab6cb90300e560f76f6b9e0025c013d38
wherculano <wagherculano@hotmail.com>
2019-06-02 07:22:41 -0300

    Encapsulando URLs da App dentro dela mesma, fazendo referencia dentro da função 'reverse' e da template-tag URL e incluindo as URLs de dentro da App com o metodo 'include'

--- pypro/urls.py
+++ pypro/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from pypro.base.views import home
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', include('pypro.base.urls')),
 ]
 
 if settings.DEBUG:
