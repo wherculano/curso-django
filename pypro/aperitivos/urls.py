9d72db331c660c557fa96ebed25e1689865aae61
Wagner <wagherculano@hotmail.com>
2019-06-08 09:26:39 -0300

    Formatando códigos (possível conclusão)

--- pypro/aperitivos/urls.py
+++ pypro/aperitivos/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from pypro.aperitivos.views import video
 
-
 app_name = 'aperitivos'
 urlpatterns = [
     path('<slug:slug>', video, name='video'),
--- pypro/urls.py
+++ pypro/urls.py
@@ -17,7 +17,6 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pypro.base.urls')),
@@ -26,4 +25,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
